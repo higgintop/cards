a1f45fc15c8405ba1cff35d99c27f4b48e0a68b9
Allison Higginbotham <higgintop@gmail.com>
2017-01-17 11:52:57 -0600

    Implemented create_deck method

--- lib/cards.ex
+++ lib/cards.ex
@@ -1,18 +1,32 @@
 defmodule Cards do
-  @moduledoc """
-  Documentation for Cards.
-  """
+  def create_deck do
+    values = ["Ace", "Two", "Three", "Four", "Five"]
+    suits = ["Spades", "Clubs", "Hearts", "Diamonds"]
 
-  @doc """
-  Hello world.
+    # List Comprehension: for loop in Elixir
+    # for every element in suits do this thing
+    # This is a mapping function, whatever is returned from do block
+    # is put in a brand new array
 
-  ## Examples
+    # SOLUTION #1
+    # cards = for value <- values do
+    #   for suit <- suits do
+    #     "#{value} of #{suit}"
+    #   end
+    # end
+    # List.flatten(cards)
 
-      iex> Cards.hello
-      :world
+    # SOLUTION #2
+    for suit <- suits, value <- values do
+        "#{value} of #{suit}"
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def shuffle(deck) do
+    Enum.shuffle(deck)
+  end
+
+  def contains?(deck, card) do
+    Enum.member?(deck, card)
   end
 end
